1e91019f3f18e27646b93668cc558504c379308c
Madeline Schimenti <madelinelise@gmail.com>
2015-10-13 19:51:37 -0400

    New A5

---
+++ madeline_schimenti_a05_cards/DeckOfCards.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madeline_schimenti_a05_cards
+{
+    public class DeckOfCards
+    {
+        private PlayingCards[] deck;
+        private int currentCard;
+        private const int NUM_CARDS = 52;
+        private Random randomNumbers;
+
+        public DeckOfCards()
+        {
+         PlayingCards.CardSuits[] faces = { PlayingCards.CardSuits.Club, PlayingCards.CardSuits.Diamond,
+                                            PlayingCards.CardSuits.Heart, PlayingCards.CardSuits.Spade };
+
+         PlayingCards.CardValues[] value = { PlayingCards.CardValues.Ace, PlayingCards.CardValues.Deuce,
+                                            PlayingCards.CardValues.Eight, PlayingCards.CardValues.Five,
+                                            PlayingCards.CardValues.Four, PlayingCards.CardValues.Jack,
+                                            PlayingCards.CardValues.King, PlayingCards.CardValues.Nine,
+                                            PlayingCards.CardValues.Queen, PlayingCards.CardValues.Seven,
+                                            PlayingCards.CardValues.Six, PlayingCards.CardValues.Ten,
+                                            PlayingCards.CardValues.Three };
+         }
+
+    }
+}
