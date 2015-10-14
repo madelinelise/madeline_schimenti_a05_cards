07403ebbc3bc8df40ea9bde54dcae54939e39c21
Madeline Schimenti <madelinelise@gmail.com>
2015-10-14 12:05:55 -0400

    added button1 function

--- madeline_schimenti_a05_cards/Form1.cs
+++ madeline_schimenti_a05_cards/Form1.cs
@@ -22,8 +22,18 @@ namespace madeline_schimenti_a05_cards
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-           
-    }
+            DeckOfCards deckOfCards = new DeckOfCards();
+            deckOfCards.Shuffle();
+            for (int i = 0; i < 52; ++i)
+            {
+
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
